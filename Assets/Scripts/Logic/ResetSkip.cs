39e45e9c102ff725beca30407f73d2cd8a8dbceb
Refrag <R3FR4GYT@gmail.com>
2023-03-17 16:49:36 +0100

    Add reset game and cutscene skip
    This patch adds a cutscene skip feature when you press A in a cutscene and then press it again to confirm, this is implemented for the 3 cutscenes present in the game.
    In the same way, it's now possible to reset back to the main menu by pressing the Back / Select button and pressing it again to confirm (this only works when you are already in the game)

---
+++ Assets/Scripts/Logic/ResetSkip.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class ResetSkip : MonoBehaviour
+{
+    public GameObject panel;
+
+    public string buttonToPress;
+
+    public string sceneToLoad;
+
+    private bool _previousPressed;
+
+    private int _confirmationTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_confirmationTimer == 0)
+            panel.SetActive(false);
+
+        bool currentPressed = Input.GetButton(buttonToPress);
+        
+        if (currentPressed != _previousPressed && currentPressed)
+        {
+            if (panel.activeSelf)
+                SceneManager.LoadScene(sceneToLoad);
+            else
+            {
+                _confirmationTimer = 180; // 3 seconds
+                panel.SetActive(true);
+            }
+        }
+
+        _previousPressed = currentPressed;
+        
+        if(_confirmationTimer > 0)
+            _confirmationTimer--;
+    }
+}
